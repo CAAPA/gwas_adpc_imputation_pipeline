d1e570a710ea35e51048590798dc08a966ee08bb
Michelle Daya <michelle.daya@gmail>
2016-02-21 19:47:36 -0700

    Removed erroneous extra bracket

--- scripts/get_dupl_merged_snps.R
+++ scripts/get_dupl_merged_snps.R
@@ -7,4 +7,4 @@ adpc.frame <- read.table(paste0(work.dir, "/b_adpc_chr", chr, ".bim"))[,c(2,4)]
 snps.excl <- adpc.frame$V2[adpc.frame$V4 %in% gwas.pos]
 
 write.table(snps.excl, paste0(work.dir, "/merged_snps_del_chr", chr, ".txt"),
-            sep="\t", quote=F, row.names=F, col.names=F))
+            sep="\t", quote=F, row.names=F, col.names=F)
