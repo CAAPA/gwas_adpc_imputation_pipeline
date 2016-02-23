d2ce185347351d8c67e1c22e606fb687c4ed7f6c
Michelle Daya <michelle.daya@ucdenver.edu>
2016-02-23 13:53:02 -0700

    Committing change to also delete SNPs where MAF > 0.3 in reference panel

--- scripts/get_AT_CG_snps.R
+++ scripts/get_AT_CG_snps.R
@@ -26,21 +26,24 @@ for (chr in 1:22) {
   #ref.freq <- read.table(paste0("../data/input/caapa_freq_chr", chr, ".txt"), 
   ref.freq <- read.table(paste0("../data/input/asw_freq_chr", chr, ".txt"), 
                          head=T,
-                         stringsAsFactors = F)
+                         stringsAsFactors = F)[,-2]
   ref.freq <- ref.freq[ ((ref.freq$A1 == "A") & (ref.freq$A2 == "T")) | 
                           ((ref.freq$A1 == "T") & (ref.freq$A2 == "A")) | 
                           ((ref.freq$A1 == "C") & (ref.freq$A2 == "G")) | 
                           ((ref.freq$A1 == "G") & (ref.freq$A2 == "C")),]
+  
   #Get the SNPs to delete - all AT/CG SNPs greater or equal than the specified MAF
   del.frame <- rbind(del.frame,
                      data.frame(SNP=chr.freq$SNP[chr.freq$MAF >= maf]))
-  #Get the SNPs to flip - all remaining AT/CG SNPs with differing minor alleles
+  #Get the SNPs to flip - remaining AT/CG SNPs with differing minor alleles
   chr.freq <- chr.freq[chr.freq$MAF < maf,]
   merged.freq <- merge(chr.freq, ref.freq, by.x=c("POS", "A1"), by.y=c("POS", "A2"))
   update.frame <- rbind(update.frame,
-                        data.frame(SNP=merged.freq$SNP.x[merged.freq$MAF.y < maf]))
+                        data.frame(SNP=merged.freq$SNP[merged.freq$MAF.y < maf]))
+  #Get more SNPs to delete - reference panel that exceeds the MAF
+  merged.freq <- merge(chr.freq, ref.freq, by.x=c("POS"), by.y=c("POS"))
   del.frame <- rbind(del.frame,
-                        data.frame(SNP=merged.freq$SNP.x[merged.freq$MAF.y >= maf]))
+                        data.frame(SNP=merged.freq$SNP[merged.freq$MAF.y >= maf]))
 }
 
 write.table(del.frame, del.file.name, sep="\t", quote=F, row.names=F, col.names=F)
