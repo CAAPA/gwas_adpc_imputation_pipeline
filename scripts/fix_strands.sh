627d1a964f31b939bab38e3845d0b363f968709c
Michelle Daya <michelle.daya@gmail>
2016-02-23 21:49:27 -0700

    Fixed file name specification

--- scripts/fix_strands.sh
+++ scripts/fix_strands.sh
@@ -25,7 +25,7 @@ plink --bfile $fix_at_cg_file_prefix \
       --make-bed --out $out_file_prefix
 
 #Report the output parameters
-snp_del_file_name=../data/working/${site}/${dataset}_AT_CG_delete.txt
+snp_flip_file_name=../data/working/${site}/${dataset}_AT_CG_update.txt
 m_at_cg=`wc -l $snp_flip_file_name | tr -s ' ' | cut -f2 -d' '`
 m_non_at_cg=`wc -l $flip_file | tr -s ' ' | cut -f2 -d' '`
 echo "m_non_at_cg_${dataset} $m_non_at_cg" >> ../data/output/${site}/flow/flow_nrs.txt
