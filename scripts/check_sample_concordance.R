aa7b4f7728a6a2085cef2bd0df42c261af79e372
Michelle Daya <michelle.daya@gmail>
2016-03-01 21:19:18 -0700

    Submitting changes to also report crossmatched samples

--- scripts/check_sample_concordance.R
+++ scripts/check_sample_concordance.R
@@ -11,6 +11,16 @@ cat(paste0("n_discordant_samples ", nr.discordant.samples, "\n"),
     file=log.file.name, append = T)
 
 if (nr.discordant.samples > 0) {
-  write.table(dupl.ibd[(dupl.ibd$PI_HAT < 0.95),c(2,4,6:10)], 
+  write.table(dupl.ibd[(dupl.ibd$PI_HAT < 0.9),c(2,4,6:10)],
+              err.file.name,  sep="\t", quote=F, row.names=F, col.names=T)
+}
+
+non.dupl.ibd <- ibd[substr(ibd$IID1,6,21) != substr(ibd$IID2,6,21),]
+nr.crossed.samples <- sum(non.dupl.ibd$PI_HAT > 0.9)
+cat(paste0("n_crossed_samples ", nr.crossed.samples, "\n"),
+    file=log.file.name, append = T)
+
+if (nr.crossed.samples > 0) {
+  write.table(non.dupl.ibd[(non.dupl.ibd$PI_HAT > 0.9),c(2,4,6:10)],
               err.file.name,  sep="\t", quote=F, row.names=F, col.names=T)
 }
