5942babc1163b1f0421799a2e2c1d925add9b055
Michelle Daya <michelle.daya@gmail>
2016-03-01 21:22:11 -0700

    Correction to write crossmatched samples to the right output file

--- scripts/check_sample_concordance.R
+++ scripts/check_sample_concordance.R
@@ -3,6 +3,7 @@ args <- commandArgs(trailingOnly = TRUE)
 ibd.file.name <- args[1]
 log.file.name <- args[2]
 err.file.name <- args[3]
+cross.file.name <- args[4]
 
 ibd <- read.table(ibd.file.name, head=T, stringsAsFactors = F)
 dupl.ibd <- ibd[substr(ibd$IID1,6,21) == substr(ibd$IID2,6,21),]
@@ -22,5 +23,5 @@ cat(paste0("n_crossed_samples ", nr.crossed.samples, "\n"),
 
 if (nr.crossed.samples > 0) {
   write.table(non.dupl.ibd[(non.dupl.ibd$PI_HAT > 0.9),c(2,4,6:10)],
-              err.file.name,  sep="\t", quote=F, row.names=F, col.names=T)
+              cross.file.name,  sep="\t", quote=F, row.names=F, col.names=T)
 }
