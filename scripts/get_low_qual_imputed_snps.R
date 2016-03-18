38abae33e963eb1b6624298402f0288d65a30a4d
Michelle Daya <michelle.daya@ucdenver.edu>
2016-03-18 17:20:44 -0600

    Comitting script used for post-imputation QC

--- scripts/get_low_qual_imputed_snps.R
+++ scripts/get_low_qual_imputed_snps.R
@@ -4,13 +4,15 @@ args <- commandArgs(trailingOnly = TRUE)
 site <- args[1]
 out.snp.file.prefix <- paste0("../data/output/", site, "/imputed_qc/snps_deleted_chr")
 out.chr.nr.file.name <- paste0("../data/output/", site, "/imputed_qc/chr_snp_count.txt")
+hist.file.name <-  paste0("../data/output/", site, "/imputed_qc/rsq_hist.pdf")
 maf.threshold <- 0.005
 lt.rsq <- 0.5
 gt.rsq <- 0.3
 cat("chr", "total_lt", "total_gt", "low_rsq_lt", "low_rsq_gt", "total\n", 
     sep="\t", file=out.chr.nr.file.name)
+rsq.all <- c()
 
-for (chr in 21:22) {
+for (chr in 1:22) {
   file.name <- paste0("../data/output/", site, "/imputed/chr", chr, ".info.gz")
   info <- read.table(gzfile(file.name), header=T, stringsAsFactors = F)
   
@@ -25,8 +27,37 @@ for (chr in 21:22) {
   
   #Write the SNPs that should be deleted to file
   out.snp.file.name <- paste0(out.snp.file.prefix, chr, ".txt")
-  cat(gsub(":", "\t", info$SNP[(info$MAF <= maf.threshold) & (info$Rsq <= lt.rsq)]),
-      sep="\n", file=out.snp.file.name)
-  cat(gsub(":", "\t", info$SNP[(info$MAF > maf.threshold) & (info$Rsq <= gt.rsq)]),
-      sep="\n", file=out.snp.file.name, append = T)
+  low.qual.str <- c(info$SNP[(info$MAF <= maf.threshold) & (info$Rsq <= lt.rsq)],
+                     info$SNP[(info$MAF > maf.threshold) & (info$Rsq <= gt.rsq)])
+  cat(gsub(":", "\t", low.qual.str), sep="\n", file=out.snp.file.name)
+
+  #Save the rsq values
+  rsq.all <- c(rsq.all, info$Rsq)
+  
+  #Get the reference frequencies
+  file.name <- paste0("../data/input/caapa_freq_chr", chr, ".txt")
+  ref.freq <- read.table(file.name, head=T, stringsAsFactors = F)
+  ref.freq <- ref.freq[!is.na(ref.freq$MAF),]
+  
+  #Remove redundant columns from info, delete low quality SNPs, and rename to merge
+  info <- info[!(info$SNP %in% low.qual.str),]
+  info$POS <- as.numeric(unlist(strsplit(info$SNP, split=":"))[seq(2,dim(info)[1]*2,2)])
+  info <- info[,c(14,2,3,4)]
+  
+  #Remove redundant columns from reference freq
+  ref.freq <- ref.freq[,-c(1,6)]
+  
+  #Merge the files, rename columns to make sense
+  merged <- merge(info, ref.freq)
+  names(merged) <- c("POS", "REF_A", "ALT_A", "SITE_F", "REF_ALT_A", "REF_REF_A",  "REF_F")     #REF_A, ALT_A is defined according to the site
+  merged <- merged[,c(1:4,6,5,7)]
+  merged$REF_F[merged$REF_A != merged$REF_ALT_A] <- 1 - merged$REF_F[merged$REF_A != merged$REF_ALT_A] 
+  
+  #Write the outped
+  write.table(merged[,-c(5,6)], paste0("../data/output/", site, "/imputed_qc/freq_chr", chr, ".txt"),  
+              sep="\t", quote=F, row.names=F, col.names=T)
+  
 }
+pdf(hist.file.name)
+hist(rsq.all, main=site, xlab="Rsq", breaks=20)
+dev.off()
--- scripts/run_post_impute_qc.sh
+++ scripts/run_post_impute_qc.sh
@@ -15,8 +15,21 @@ mkdir ../data/output/${site}/imputed_qc
 cat get_low_qual_imputed_snps.R | R --vanilla --args $site
 
 #Delete those SNPs
-for ((chr=21; chr<=22; chr++)); do
+total_kept_snps=0
+for ((chr=1; chr<=22; chr++)); do
     vcftools --gzvcf ../data/output/${site}/imputed/chr${chr}.dose.vcf.gz \
              --exclude-positions ../data/output/${site}/imputed_qc/snps_deleted_chr${chr}.txt \
-             --recode --stdout | gzip -c > ../data/output/${site}/imputed_qc/chr${chr}
+             --recode --stdout | gzip -c > ../data/output/${site}/imputed_qc/chr${chr}.dose.vcf.gz
+    kept_snps=`grep "After filtering, kept" out.log | grep "Sites" | cut -f4 -d' '`
+    total_kept_snps=$(($total_kept_snps+$kept_snps))
+    rm out.log
 done
+
+n_cols=`zcat < ../data/output/${site}/imputed_qc/chr22.dose.vcf.gz | head -10 | tail -1 | wc -w`
+n_imputed=$(($n_cols-9))
+echo "n_imputed $n_imputed" >> ../data/output/${site}/flow/flow_nrs.txt
+m_imputed=`tail -22 chr_snp_count.txt | cut -f6 | paste -sd+ - | bc`
+echo "m_imputed $m_imputed" >> ../data/output/${site}/flow/flow_nrs.txt
+echo "m_qc_imputed $total_kept_snps" >> ../data/output/${site}/flow/flow_nrs.txt
+m_qc_del_impute=`wc -l ../data/output/${site}/imputed_qc/snps_deleted_chr* | tail -1 | tr -s ' ' | cut -f2 -d' '`
+echo "m_qc_del_impute $m_qc_del_impute" >> ../data/output/${site}/flow/flow_nrs.txt
