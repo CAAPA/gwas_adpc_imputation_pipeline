acf8940b9ca79d59517ecc44fa3115476417a4a9
Michelle Daya <michelle.daya@ucdenver.edu>
2016-03-22 15:36:01 -0600

    Script to clean the working directories

---
+++ scripts/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Set parameters
+if [ "$#" -eq  "0" ]
+then
+    echo "Usage: ${0##*/} <site>"
+    exit
+fi
+
+cp ../data/working/${site}/discordant_samples.txt ../data/output/${site}
+cp ../data/working/${site}/crossmatched_samples.txt ../data/output/${site}
+
+rm -r ../data/working/${site}
