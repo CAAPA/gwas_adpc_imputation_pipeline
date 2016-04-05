4aba1d1d5b95925acfb5de93da9be3b69297c0e5
Michelle Daya <michelle.daya@ucdenver.edu>
2016-04-05 14:10:52 -0600

    Fixed script to use bgzip to create output files for imputation server

--- scripts/mask_gwas_snps.sh
+++ scripts/mask_gwas_snps.sh
@@ -19,5 +19,11 @@ cat get_snps_to_mask.R | R --vanilla --args $site
 for ((chr=1; chr<=22; chr++)); do
     vcftools --gzvcf ../data/output/${site}/merged/chr${chr}.vcf.gz \
              --exclude-positions ../data/output/${site}/masked/snp_pos_chr${chr}.txt \
-             --recode --stdout | gzip -c > ../data/output/${site}/masked/typed/chr${chr}.vcf.gz
+             --recode --stdout  > ../data/output/${site}/masked/typed/chr${chr}.vcf
+
+    vcf-sort ../data/output/${site}/masked/typed/chr${chr}.vcf | \
+        bgzip -c > \
+              ../data/output/${site}/masked/typed/chr${chr}.vcf.gz
 done
+
+rm  ../data/output/${site}/masked/typed/*.vcf
