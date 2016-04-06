1e3bbd1e6ac5e3d97477776f912bc8c473fd6dd2
Michelle Daya <michelle.daya@ucdenver.edu>
2016-04-06 16:53:35 -0600

    Reverted changes - this is to run on CAAPA

--- scripts/mask_only_gwas_array_snps.sh
+++ scripts/mask_only_gwas_array_snps.sh
@@ -4,7 +4,7 @@
 if [ "$#" -eq  "0" ]
 then
     echo "Usage: ${0##*/} <site>"
-    echo "Script must ONLY be run after mask_gwas_snps_flip_tgp.sh"
+    echo "Script must ONLY be run after mask_gwas_snps.sh"
     exit
 fi
 
@@ -16,19 +16,13 @@ mkdir ../data/output/${site}/masked/typed_gwas_array_only
 mkdir ../data/output/${site}/masked/imputed_gwas_array_only
 
 work_dir=../data/working/${site}
-flip_file=../data/working/${site}/tgp_flip.txt
 
 for ((chr=1; chr<=22; chr++)); do
     vcftools --vcf ${work_dir}/gwas_fixed_chr${chr}.vcf \
              --exclude-positions ../data/output/${site}/masked/snp_pos_chr${chr}.txt \
-             --recode --stdout  > ../data/working/${site}/masked_gwas_chr${chr}.vcf
+             --recode --stdout  > ../data/output/${site}/masked/typed_gwas_array_only/chr${chr}.vcf
 
-    plink --vcf ../data/working/${site}/masked_gwas_chr${chr}.vcf \
-          --flip ${flip_file} \
-          --recode vcf \
-          --out ../data/working/${site}/flipped_masked_gwas_chr${chr}
-
-    vcf-sort  ../data/working/${site}/flipped_masked_gwas_chr${chr}.vcf | \
+    vcf-sort ../data/output/${site}/masked/typed_gwas_array_only/chr${chr}.vcf | \
         bgzip -c > \
               ../data/output/${site}/masked/typed_gwas_array_only/chr${chr}.vcf.gz
 done
