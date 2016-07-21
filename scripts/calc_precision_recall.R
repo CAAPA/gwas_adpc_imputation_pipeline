89fa904e18845ad415e4fbcff7b64890959013ef
Michelle Daya <michelle.daya@ucdenver.edu>
2016-07-21 17:14:58 -0600

    Changes to also calculate precision and recall

--- scripts/calc_precision_recall.R
+++ scripts/calc_precision_recall.R
@@ -4,7 +4,7 @@ site <- args[1] #"jhu_abr"
 typed.file <- args[2] #"masked_typed_genos.vcf"
 imputed.file <- args[3] #"masked_imputed_genos.vcf"
 imputed.dir <- args[4] #"imputed"
-out.file <- args[5] #accuracies.txt
+out.file <- args[5] #precision_recall.txt
 
 out.file <- paste0("../data/output/", site, "/masked/", out.file)
 typed.file <- paste0("../data/output/", site, "/masked/", typed.file)
@@ -17,6 +17,7 @@ for (chr in 1:22) {
                              head=T, stringsAsFactors = F)[,c(1,5,7)]
   markers <- read.table(paste0("../data/output/masked/snp_pos_chr", chr, ".txt"))
   chr.snp.info <- chr.snp.info[chr.snp.info$SNP %in% paste(markers$V1, markers$V2, sep=":"),]
+  chr.snp.info <- chr.snp.info[chr.snp.info$MAF < 0.05,]
   snp.info <- rbind(snp.info, chr.snp.info)
 }
 
@@ -24,19 +25,35 @@ typed.frame <- read.delim(typed.file, stringsAsFactors = F)
 imputed.frame <- read.delim(imputed.file, stringsAsFactors = F)
 imputed.snps <- paste(imputed.frame$CHROM, imputed.frame$POS, sep=":")
 imputed.frame <- imputed.frame[!duplicated(imputed.snps),]
-
-cat("SNP\tMAF\tRsq\tprop_match\n", file=out.file)
+cat("SNP\tMAF\tRsq\tprecision\trecall\n", file=out.file)
 if (identical(typed.frame$POS, imputed.frame$POS)) {
   cols <- 5:dim(typed.frame)[2] 
   n <- dim(typed.frame)[2] - 4
   for (r in 1:dim(typed.frame)[1]) {
     typed.row <- typed.frame[r,cols]
-    imputed.row <- imputed.frame[r,cols]
-    prop.match <- sum(typed.row == imputed.row)/n
     snp <- paste(typed.frame$CHROM[r], typed.frame$POS[r], sep=":")
-    maf <- snp.info$MAF[snp.info$SNP == snp]
-    rsq <- snp.info$Rsq[snp.info$SNP == snp]
-    cat(snp, maf, rsq, paste0(prop.match, "\n"), sep="\t", file=out.file, append = T)
+    if ((snp %in% snp.info$SNP) & !(is.na(typed.row[1,1]))) {
+      imputed.row <- imputed.frame[r,cols]
+      maf <- snp.info$MAF[snp.info$SNP == snp]
+      rsq <- snp.info$Rsq[snp.info$SNP == snp]
+      typed.rare <- rep(0, n)
+      imputed.rare <- rep(0, n)
+      if (sum(typed.row == "0/0") < sum(typed.row == "1/1")) {
+        typed.rare[typed.row == "0/0"] <- 1
+        imputed.rare[imputed.row == "0/0"] <- 1
+      } else {
+        typed.rare[typed.row == "1/1"] <- 1
+        imputed.rare[imputed.row == "1/1"] <- 1  
+      }
+      typed.rare[typed.row == "0/1"] <- 1
+      imputed.rare[imputed.row == "0/1"] <- 1
+      true.pos <- sum((typed.rare == 1) & (imputed.rare == 1))
+      false.pos <- sum((typed.rare == 0) & (imputed.rare == 1))
+      false.neg <- sum((typed.rare == 1) & (imputed.rare == 0))
+      precision <- true.pos/(true.pos + false.pos)
+      recall <- true.pos/(true.pos + false.neg)
+      cat(snp, maf, rsq, precision, paste0(recall, "\n"), sep="\t", file=out.file, append = T)
+    }
   }
 } else { #Do not process if SNP positions are not identical
   cat("ERROR! SNP positions not identical", file = out.file)
--- scripts/get_masked_accuracies.sh
+++ scripts/get_masked_accuracies.sh
@@ -4,18 +4,22 @@
 #bash extract_masked_snps.sh jhu_abr imputed
 #python extract_masked_genos.py jhu_abr masked_imputed masked_typed
 #cat compare_masked_genos.R | R --vanilla --args jhu_abr masked_typed_genos.vcf masked_imputed_genos.vcf imputed accuracies.txt
+#cat calc_precision_recall.R | R --vanilla --args jhu_abr masked_typed_genos.vcf masked_imputed_genos.vcf imputed precision_recall.txt
 
 #bash extract_masked_snps.sh ucsf_pr typed
 #bash extract_masked_snps.sh ucsf_pr imputed
-python extract_masked_genos.py ucsf_pr masked_imputed masked_typed
-cat compare_masked_genos.R | R --vanilla --args ucsf_pr masked_typed_genos.vcf masked_imputed_genos.vcf imputed accuracies.txt
+#python extract_masked_genos.py ucsf_pr masked_imputed masked_typed
+#cat compare_masked_genos.R | R --vanilla --args ucsf_pr masked_typed_genos.vcf masked_imputed_genos.vcf imputed accuracies.txt
+cat calc_precision_recall.R | R --vanilla --args ucsf_pr masked_typed_genos.vcf masked_imputed_genos.vcf imputed precision_recall.txt
 
 #bash extract_masked_snps.sh washington typed
 #bash extract_masked_snps.sh washington imputed
-python extract_masked_genos.py washington masked_imputed masked_typed
-cat compare_masked_genos.R | R --vanilla --args washington masked_typed_genos.vcf masked_imputed_genos.vcf imputed accuracies.txt
+#python extract_masked_genos.py washington masked_imputed masked_typed
+#cat compare_masked_genos.R | R --vanilla --args washington masked_typed_genos.vcf masked_imputed_genos.vcf imputed accuracies.txt
+cat calc_precision_recall.R | R --vanilla --args washington masked_typed_genos.vcf masked_imputed_genos.vcf imputed precision_recall.txt
 
 #bash extract_masked_snps.sh washington typed_tgp
 #bash extract_masked_snps.sh washington imputed_tgp
-python extract_masked_genos.py washington masked_imputed_tgp masked_typed_tgp
+#python extract_masked_genos.py washington masked_imputed_tgp masked_typed_tgp
 cat compare_masked_genos.R | R --vanilla --args washington masked_typed_tgp_genos.vcf masked_imputed_tgp_genos.vcf imputed_tgp accuracies_tgp.txt
+cat calc_precision_recall.R | R --vanilla --args washingon masked_typed_tgp_genos.vcf masked_imputed_tgp_genos.vcf imputed_tgp precision_recall_tgp.txt
